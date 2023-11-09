26d0e1c3a6d34ba0fb0c981290e8dc9b73a145de
dylanebert <dylan@huggingface.co>
2023-11-08 21:45:39 -0500

    add example

--- demo/app.py
+++ demo/app.py
@@ -4,7 +4,7 @@ from gradio_model3dgs import Model3DGS
 
 
 with gr.Blocks() as demo:
-    Model3DGS()
+    Model3DGS(examples=["https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k.splat"])
 
 
 demo.launch()
